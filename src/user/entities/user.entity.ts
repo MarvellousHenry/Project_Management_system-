147efcac2d41f5299923265424bf4cf72ae14b4d
Marvellous Henry <marvellousekemini@gmail.com>
2025-01-16 13:39:33 +0100

    first commit

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { userRole } from "../enum/user.role.enum";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column({unique: true})
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column({
+        type: 'enum',
+        enum: userRole.user,
+        default: userRole.user
+    })
+    role: userRole;
+}
